133a97ec2b5385a5adf55aba970a1a74b9dc6d5d
Cindy Burton <cindy.brton@gmail.com>
2020-03-15 16:30:22 -0600

    it works and I even know why

--- models/workout.js
+++ models/workout.js
@@ -1,35 +1,60 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
+const workoutSchema = new Schema(
+  {
     day: {
-        type: Date,
-        default: Date.now,
-        },
+      type: Date,
+      default: () => new Date()
+    },
     exercises: [
-        {
-            type: {
-                type: String,
-                // required: "Cardio or resistance?",
-            },
-            name: {
-                type: String,
-                // required: "What workout did you do?",
-                trim: true
-            },
-            duration: {
-                types: Number,
-                // required: "How long did you work out?"
-            },
-            weight: Number,
-            reps: Number,
-            set: Number,
-            distance: Number
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
         }
-    ]  
+      }
+    ]
+  },
+  {
+    toJSON: {
+      // include any virtual properties when data is requested
+      virtuals: true
+    }
+  }
+);
+
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+  // "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,14 @@
 const { Workout } = require('../models');
 const router = require('express').Router();
 
+router.post('/api/workouts', (req, res) => {
+    console.log('req', req)
+    // console.log('post req.body', req.body);
+    Workout.create({}).then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+})
+
 router.get('/api/workouts', (req, res) => {
     Workout.find().then(dbWorkout => {
         res.json(dbWorkout)
@@ -9,15 +17,16 @@ router.get('/api/workouts', (req, res) => {
     })
 });
 
-router.get('/api/workouts/:id', ({ body }, res) => {
-    console.log('body.id', body.id);
-    Workout.find({ _id: body.id }).then(dbWorkout => {
+router.put('/api/workouts/:id', (req, res) => {
+    console.log('put req.body', req.body);
+    console.log('put req.params', req.params.id);
+    Workout.update({ _id: req.params.id }, {$push: {exercises: req.body}}).then(dbWorkout => {
         res.json(dbWorkout);
     })
 });
 
 router.get('/api/workouts/range', (req, res) => {
-    Workout.find().sort({ day: 1 }).then(dbWorkout => {
+    Workout.find().sort({ day: -1 }).then(dbWorkout => {
         res.json(dbWorkout);
     }).catch(err => {
         console.log(err);
